aab2f370ddb2f75800ee5a873f6e87ae2a2c2ffe
Gopal Sharma <gopalenggr@gmail.com>
2023-06-02 10:13:18 -0600

    added updated file for eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+      console.log(" ✅✅✅ Assertion passed : " + actual + " === " + expected);
+  }
+  else {
+      console.log(" 🛑🛑🛑 Assertion failed : " +  actual +" !== "  +  expected);
+  }   
+
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if ( arr1.length !== arr2.length) {
+    return false;
+  }
+  for ( let i= 0; i < arr1.length; i++)  {
+    if(arr1[i] !== arr2[i] ) {
+      return false;
+    }
+  }
+  return true;
+
+
+};
+
+// TEST CODE to check
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
